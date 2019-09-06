331e6b3fe021b8135b5a8dbcb52e5ac812cd93fc
fdgifford3 <fdgifford3@gmail.com>
2019-09-06 17:39:43 -0500

    Week 2 Factory Pattern
    Week 2 Factory Pattern Main Class

---
+++ FactoryWeek2FGifford/src/FactoryWeek2FGifford.java
@@ -0,0 +1,15 @@
+/*
+ * Frederick Gifford
+ * CPSC-60000 Week 2 Factory Pattern to Extend
+ * Week 1 Decorator Pattern with Baking Theme
+ * 9.6.2019
+ * Prof Nowak
+ */
+public class FactoryWeek2FGifford {
+
+	public static void main(String[] args) {
+		System.out.println("Frederick Gifford\nCPSC-60000\nWeek 2 Factory Pattern\n");
+
+	}
+
+}
